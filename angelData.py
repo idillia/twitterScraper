ffce1a01465fa0627d74114936cb94c85bf1f0db
idillia <mila.antonova@gmail.com>
2016-11-11 08:44:32 -0800

    add anglelist

---
+++ angelData.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import configAngel
+from configAngel import _user, _pw
+import csv
+import selenium
+from selenium import webdriver
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time 
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+
+def findTwitterHandler(path):	
+			
+	profileName = driver.find_element_by_xpath(path).text
+	profile = driver.find_element_by_xpath(path).get_attribute("href")
+
+	main_window = driver.current_window_handle
+
+	print "Profile XpathString: %s," % (profileXpathString)     
+	print "Profile name: %s," % (profileName)
+	print "Open window: %s," % (main_window)
+	
+	script = '''window.open("%s", "Whatever")''' % (profile)
+
+	driver.execute_script(script)
+	next_window = driver.window_handles[1]
+	driver.switch_to_window(next_window)
+	
+
+	print "All windows: %s," % (driver.window_handles)
+	print "Swtched to window: %s," % (next_window)
+	print "Current window: %s," % driver.current_window_handle
+
+	driver.execute_script(script)
+	try: 
+		handler = driver.find_element_by_css_selector("*[class^='icon link_el fontello-twitter']")
+		twitterLink = handler.get_attribute("href")
+		print(twitterLink)
+		
+		with open("handlersData.csv", "a") as scrapedDataFile:
+			scrapedDataFileWriter = csv.writer(scrapedDataFile)
+			scrapedDataFileWriter.writerow([profileName, twitterLink])
+
+		scrapedDataFile.close()
+		print "Profile saved"
+		driver.close()
+	except NoSuchElementException: 
+		driver.close()   
+
+	driver.switch_to_window(main_window)
+	sleep(2) #wait until new tab finishes loading
+	return;
+
+
+
+chrome_path = r"/Users/mila/Downloads/chromedriver"
+driver = webdriver.Chrome(chrome_path)
+
+driver.set_page_load_timeout(30)
+driver.get("https://angel.co/people/all")	
+driver.implicitly_wait(20)
+
+moreBtn = driver.find_element_by_css_selector("*[class^='more_link u-unstyledLink u-textShadowWhite']").click()
+print "Button is clicked %s " % (moreBtn)
+driver.set_page_load_timeout(30)
+
+
+for x in range(1,36):
+	if x <= 12:
+		profileXpathString = '''//*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[%s]/div/div[1]/div/div[2]/div[1]/a''' % (x)
+		print "Loop counter:  %d" % (x)
+		findTwitterHandler(profileXpathString)
+		
+	elif x > 12 and x <= 24:
+		for y in range(1, 13):
+			profileXpathString = '''//*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[13]/div[%s]/div/div[1]/div/div[2]/div[1]/a''' % (y)
+			print(profileXpathString)
+			findTwitterHandler(profileXpathString)
+	elif x > 24 and x <= 36:
+		for c in range(1, 13):
+			profileXpathString = '''//*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[13]/div[13]/div[%s]/div/div[1]/div/div[2]/div[1]/a''' % (c)
+			print(profileXpathString)
+			findTwitterHandler(profileXpathString)
+
+	
+# //*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[12]/div/div[1]/div/div[2]/div[1]/a
+
+# //*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[13]/div[12]/div/div[1]/div/div[2]/div[1]/a
+
+# //*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[13]/div[13]/div[12]/div/div[1]/div/div[2]/div[1]/a
+
+# //*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[13]/div[13]/div[13]/div[11]/div/div[1]/div/div[2]/div[1]/a
+# //*[@id="root"]/div[4]/div[2]/div[1]/div/div/div[2]/div/div[2]/div/div[13]/div[13]/div[13]/div[12]/div/div[1]/div/div[2]/div[1]/a
+
+
--- handlerSave.py
+++ handlerSave.py
@@ -3,6 +3,9 @@ from config import _user, _pw
 import csv
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
 chrome_path = r"/Users/mila/Downloads/chromedriver"
 driver = webdriver.Chrome(chrome_path)
 
@@ -16,30 +19,76 @@ driver.find_element_by_id("loginbutton").click()
 
 driver.implicitly_wait(120)
 
-driver.get("https://www.quora.com/profile/Pei-Patrick-Kuo")
+# driver.get("https://www.quora.com/profile/Pei-Patrick-Kuo")
+driver.get("https://www.quora.com/topic/Company-Culture/writers")
 driver.implicitly_wait(20)
-driver.find_element_by_css_selector("*[class^='header_signin_with_search_bar action_button']").click()
+
+# driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+
+# driver.find_element_by_xpath('//body').send_keys(Keys.CONTROL+Keys.END)
+# elm = driver.find_element_by_tag_name('html')
+# elm.send_keys(Keys.END)
+# time.sleep(8)
+# driver.find_element_by_css_selector("*[class^='header_signin_with_search_bar']").click()
+# driver.implicitly_wait(20)
+# list = driver.find_elements_by_css_selector("*[class='user']")
+
+list = driver.find_element_by_css_selector("a[href*='profile']")
+print(list)
+print(list.text)
+# listId = driver.find_elements_by_id("_name_link")
+# listId1 = driver.find_elements_by_xpath('//div[contains(., "name_link")]')
 
 
-handler = driver.find_element_by_xpath("""//*/div/div[4]/a""")
+# print(list)
+# driver.implicitly_wait(40)
 
-twitterLink = handler.get_attribute("href")
-print(twitterLink)
+# for el in list:
+# 	# if len (el.text) !=0:
+# 	driver.implicitly_wait(5)
+# 	print(el)
+# 	print(el.text)
+# 	print(el.get_attribute("href"))
+		# el.click()
+
+		# el.click()
+		
+# print(list[4])
+# list[4].click()
 
 # driver.quit()
-with open("handlersData.csv", "r") as scrapedDataFile:
-	scrapedDataFileReader = csv.reader(scrapedDataFile)
-	scrapedList = []
-	for row in scrapedDataFileReader:
-		if len (row) != 0:
-			scrapedList = scrapedList + [row]		
+# list[1].click()
 
-scrapedDataFile.close()
+# handler = driver.find_element_by_xpath("""//*/div/div[4]/a""")
 
-print(scrapedList)	
+# twitterLink = handler.get_attribute("href")
+# print(twitterLink)
 
-with open("handlersData.csv", "a") as scrapedDataFile:
-	scrapedDataFileWriter = csv.writer(scrapedDataFile)
-	scrapedDataFileWriter.writerow(['/n', twitterLink])
 
-scrapedDataFile.close()
+
+# .get_attribute("href")
+
+
+# driver.quit()	
+# with open("handlersData.csv", "r") as scrapedDataFile:
+# 	scrapedDataFileReader = csv.reader(scrapedDataFile)
+# 	scrapedList = []
+# 	for row in scrapedDataFileReader:
+# 		if len (row) != 0:
+# 			scrapedList = scrapedList + [row]		
+
+# scrapedDataFile.close()
+
+
+# print(scrapedList)	
+
+# with open("handlersData.csv", "a") as scrapedDataFile:
+# 	scrapedDataFileWriter = csv.writer(scrapedDataFile)
+# 	scrapedDataFileWriter.writerow(['/n', twitterLink])
+
+# scrapedDataFile.close()
+
+
+# Actions action = new Actions(webdriver);
+# WebElement we = webdriver.findElement(By.xpath("html/body/div[13]/ul/li[4]/a"));
+# action.moveToElement(we).moveToElement(webdriver.findElement(By.xpath("/expression-here"))).click().build().perform();
